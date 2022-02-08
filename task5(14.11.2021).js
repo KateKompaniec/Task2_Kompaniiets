83dac18730d433c41060e1f3157cb04775f64a3f
Catherine Kompaniiets <55803215+KateKompaniec@users.noreply.github.com>
2022-02-08 15:57:26 +0200

    Update task5(14.11.2021).js

--- task5(14.11.2021).js
+++ task5(14.11.2021).js
@@ -52,4 +52,45 @@ result => console.log(result)
 ).catch((error)=>{
 	console.log(error)});
 
-  
+// 5
+const seconds1= Math.floor((Math.random()*4)+1)
+const promise1 = new Promise((resolve, reject) => {
+setTimeout(() => {
+resolve(seconds1)},seconds1*1000);
+})
+  const seconds2= Math.floor((Math.random()*4)+1)
+const promise2 = new Promise((resolve, reject) => {
+setTimeout(() => {
+resolve(seconds2)},seconds2*1000);
+})
+const seconds3= Math.floor((Math.random()*4)+1)
+const promise3 = new Promise((resolve, reject) => {
+setTimeout(() => {
+resolve(seconds3)},seconds3*1000);
+})
+
+Promise.all([promise1,promise2,promise3])
+.then(result=>{ console.log(result)
+	let sum=0
+	for(let i=0;i<result.length;i++)
+		{sum+=result[i]}
+    console.log("Sum is "+sum)},reason=>{
+console.log(reason)
+})  
+ // 5 var2
+var promise= new Array(3);
+for(let i=0; i<promise.length;i++){
+const seconds1= Math.floor((Math.random()*4)+1)
+ promise[i] = new Promise((resolve, reject) => {
+setTimeout(() => {
+resolve(seconds1)},seconds1*1000);
+})
+}
+Promise.all(promise)
+.then(result=>{ console.log(result)
+	let sum=0
+	for(let i=0;i<result.length;i++)
+		{sum+=result[i]}
+    console.log("Sum is "+sum)},reason=>{
+console.log(reason)
+})
